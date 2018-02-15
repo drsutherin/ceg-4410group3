016403c089c621e74538e8b49ce2714c61a129dd
David Sutherin <sutherin.3@wright.edu>
2018-02-15 10:34:06 -0500

    Adds empty HelpActivity

---
+++ app/src/main/java/ceg4410/wright/edu/myapplication/HelpActivity.java
@@ -0,0 +1,21 @@
+package ceg4410.wright.edu.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
+
+
+/**
+ * Created by David Sutherin on 2/15/2018.
+ */
+
+class HelpActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
