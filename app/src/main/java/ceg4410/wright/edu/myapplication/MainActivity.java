efb2361c29b691b7bb3ab520f5d74979e9230cc7
Don Miller <31510926+Miller189@users.noreply.github.com>
2018-02-15 09:54:00 -0500

    Added openHelpScreen

--- app/src/main/java/ceg4410/wright/edu/myapplication/MainActivity.java
+++ app/src/main/java/ceg4410/wright/edu/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package ceg4410.wright.edu.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openHelpScreen(View view){
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
     }
 }
