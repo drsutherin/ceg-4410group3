a1fd1aae12aa6c17a163486b952172735ddcb2fb
David Sutherin <sutherin.3@wright.edu>
2018-03-28 11:38:09 -0400

    Adds empty lobby and main activities

---
+++ app/src/main/java/ceg4410/wright/edu/myapplication/TeacherLobby.java
@@ -0,0 +1,13 @@
+package ceg4410.wright.edu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherLobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_lobby);
+    }
+}
