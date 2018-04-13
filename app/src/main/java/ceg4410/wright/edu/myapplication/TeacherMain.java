46cd774a7233e2d5704a0e58ff38f1544ea98500
Don Miller <31510926+Miller189@users.noreply.github.com>
2018-04-13 11:48:45 -0400

    working speech-to-text code

--- app/src/main/java/ceg4410/wright/edu/myapplication/TeacherMain.java
+++ app/src/main/java/ceg4410/wright/edu/myapplication/TeacherMain.java
@@ -1,3 +1,10 @@
+/*
+ *  Much of this code was shamelessly copied from
+ *  http://stacktips.com/tutorials/android/speech-to-text-in-android
+ *  by Don Miller
+ *
+ */
+
 package ceg4410.wright.edu.myapplication;
 
 import android.content.ActivityNotFoundException;
@@ -16,7 +23,6 @@ public class TeacherMain extends AppCompatActivity {
 
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
-    private Button mSpeakBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,7 @@ public class TeacherMain extends AppCompatActivity {
 
         mVoiceInputTv = (TextView) findViewById(R.id.voiceInput);
         mVoiceInputTv.setMovementMethod(new ScrollingMovementMethod());
-        mSpeakBtn = (Button) findViewById(R.id.btnSpeak);
+        Button mSpeakBtn = (Button) findViewById(R.id.btnSpeak);
         mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -35,19 +41,15 @@ public class TeacherMain extends AppCompatActivity {
         });
     }
 
-    /*
-    *  http://stacktips.com/tutorials/android/speech-to-text-in-android
-    *
-    *
-     */
+
     private void startVoiceInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Talk to the phone!");
         try {
             startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
+        } catch (ActivityNotFoundException ignored) {
 
         }
     }
@@ -61,6 +63,7 @@ public class TeacherMain extends AppCompatActivity {
                 if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     mVoiceInputTv.setText(result.get(0));
+                    // Send result to student device
                 }
                 break;
             }
